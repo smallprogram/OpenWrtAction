ba4b7da696b203007e52056642542f3ee8c32a18
zhusir <smallprogram@foxmail.com>
2021-08-22 20:35:20 +0800

    test add new theme

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 添加新主题
+cd package/lean  
+rm -rf luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git 
