fb35ff9bec1f1b497b26670b7e9bb0950ad25da0
zhusir <smallprogram@foxmail.com>
2021-10-04 00:31:50 +0800

    update config commit message

--- wsl2op.sh
+++ wsl2op.sh
@@ -454,7 +454,7 @@ then
 
     if [ -n "$(git status -s)" ]; then 
         git add .
-        git commit -m "update from local"
+        git commit -m "update $configName from local bash"
         git push origin
         echo -e "\033[31m 已将新配置的config同步回OpenwrtAction \033[0m"
         sleep 2s
