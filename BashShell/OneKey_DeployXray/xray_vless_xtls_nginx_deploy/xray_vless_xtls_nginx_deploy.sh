f41ff0e7eb6d216cedf6955df3d1376168f82004
zhusir <smallprogram@foxmail.com>
2021-10-21 02:23:38 +0800

    update

--- BashShell/OneKey_DeployXray/xray_vless_xtls_nginx_deploy/xray_vless_xtls_nginx_deploy.sh
+++ BashShell/OneKey_DeployXray/xray_vless_xtls_nginx_deploy/xray_vless_xtls_nginx_deploy.sh
@@ -1,3 +1,48 @@
+#!/bin/bash
+
+
+acmeEmail
+domainName
+timer=15
+
+# acme
+function acmeinit(){
+    apt-get -y install socat
+    echo -e "\033[31m 请输入用于注册acme的Email，注意不要输入无效的Email，否则将无法获取证书！"
+    read acmeEmail
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 安装acme依赖
+apt-get update 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 cat config.json | jq '.inbounds[0].settings.clients += [{"id":"12312","flow":"ddd",level:0,"email":"ccc"}]' >> test.json
 
 
