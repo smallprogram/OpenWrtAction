f57d3b2bb8bba26c7318a0aebbcc8e2a9c79979d
小尨 <43496459+Jason6111@users.noreply.github.com>
2022-03-08 21:39:36 +0800

    Update wsl2op.sh

--- wsl2op.sh
+++ wsl2op.sh
@@ -61,7 +61,21 @@ function LogMessage(){
 }
 
 LogMessage "\033[34m 注意，请确保当前linux账户为非root账户，并且已经安装相关编译依赖 \033[0m" "\033[34m Note, please make sure that the current linux account is a non-root account, and the relevant compilation dependencies have been installed \033[0m"
-LogMessage "\033[34m 如果不符合上述条件，请ctrl+C退出 \033[0m" "\033[34m If the above conditions are not met, please ctrl+C to exit \033[0m"
+LogMessage "\033[34m 如果不符合上述条件，请安装依赖或ctrl+C退出 \033[0m" "\033[34m If the above conditions are not met, please Install dependencies or ctrl+C to exit \033[0m"
+LogMessage "\033[31m 是否安装编译依赖，不输入默认不安装，输入任意值安装，将会在$timer秒后自动选择默认值 \033[0m" "\033[31m Whether to install the compilation dependencies. Do not enter the default. Do not install. Enter any value to install. The default value will be automatically selected after $timer seconds \033[0m"
+    read -t $timer dependencies
+    if [ ! -n "$dependencies" ]; then
+        LogMessage "\033[34m OK，不安装 \033[0m" "\033[34m OK, Not installed \033[0m"   
+    else
+        LogMessage "\033[34m 开始安装 \033[0m" "\033[34m Start installation \033[0m"
+        sudo apt-get update
+        sleep 10s
+        sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+        sleep 10s
+        git config --global http.sslverify false
+        git config --global https.sslverify false
+        LogMessage "\033[34m 安装完成 \033[0m" "\033[34m Copy completed \033[0m" 
+    fi
 
 # 将编译的固件提交到GitHubRelease
 # function UpdateFileToGithubRelease(){
