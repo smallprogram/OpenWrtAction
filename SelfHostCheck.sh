433aedbde3ea3cc7695560fa01880e21d05b6db8
zhusir <smallprogram@foxmail.com>
2022-08-14 05:19:54 +0800

    Update

--- SelfHostCheck.sh
+++ SelfHostCheck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /home/zhusir
-if [ ! -d "/home/zhusir/OpenWrtAction" ]; then git clone https://github.com/smallprogram/OpenWrtAction.git; else cd /home/zhusir/OpenWrtAction; git stash; git stash drop; git pull; fi;
+cd /home/$USER
+if [ ! -d "/home/$USER/OpenWrtAction" ]; then git clone https://github.com/smallprogram/OpenWrtAction.git; else cd /home/$USER/OpenWrtAction; git stash; git stash drop; git pull; fi;
