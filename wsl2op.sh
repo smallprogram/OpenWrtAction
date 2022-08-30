63223f985f9f691971435c6265af6a3971a2d083
zhusir <smallprogram@foxmail.com>
2022-08-30 13:10:25 +0800

    update wsl2op.sh

--- wsl2op.sh
+++ wsl2op.sh
@@ -396,10 +396,11 @@ function Func_Main(){
             Func_LogMessage "\033[34m OK，不安装 \033[0m" "\033[34m OK, Not installed \033[0m"   
         else
             Func_LogMessage "\033[34m 开始安装 \033[0m" "\033[34m Start installation \033[0m"
-            sudo apt-get update
-            sleep 10s
-            sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
-            sleep 10s
+            sudo apt update -y
+            sudo apt full-upgrade -y
+            sleep 5s
+            sudo apt-get -y install ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip libpython3-dev qemu-utils rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+            sleep 5s
             git config --global http.sslverify false
             git config --global https.sslverify false
             Func_LogMessage "\033[34m 安装完成 \033[0m" "\033[34m Installation Completed \033[0m" 
