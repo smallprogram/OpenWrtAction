ac02c1b97a23fd5e206e5dc20e6dc8028d151529
zhusir <smallprogram@foxmail.com>
2023-02-17 13:28:46 +0800

    update diy-part1.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2023 SmallProgram <https://github.com/smallprogram>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
+# https://github.com/smallprogram/OpenWrtAction
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
