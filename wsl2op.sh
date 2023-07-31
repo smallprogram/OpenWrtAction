68503711ad638a57a4ab4765177fd94f17a33a97
zhusir <smallprogram@foxmail.com>
2023-08-01 02:29:28 +0800

    update wsl2op.sh change depend online

--- wsl2op.sh
+++ wsl2op.sh
@@ -654,7 +654,7 @@ function Func_Main(){
             sudo apt update -y
             sudo apt full-upgrade -y
             sleep 5s
-            sudo apt-get -y install ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip libpython3-dev qemu-utils rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+            sudo apt-get -y install $(curl -fsSL https://github.com/smallprogram/OpenWrtAction/raw/main/diy_script/depends)
             sleep 5s
             git config --global http.sslverify false
             git config --global https.sslverify false
