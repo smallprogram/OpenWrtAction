e3fe0dd9b344c08063c8232b71754c1cc5b5128d
zhusir <smallprogram@foxmail.com>
2024-01-15 23:14:57 +0800

    test

--- wsl2op_openwrt.sh
+++ wsl2op_openwrt.sh
@@ -591,6 +591,7 @@ function Func_Main(){
     cd /home/${user_name}
     if [ ! -d "/home/${user_name}/${openwrt_dir}" ];
     then
+        echo "git clone $openwrt_source ${openwrt_dir}"
         git clone $openwrt_source ${openwrt_dir}
         cd /home/${user_name}
         is_first_compile=1
