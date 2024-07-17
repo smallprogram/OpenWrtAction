aede91622377baff416ed0f445606d8c38273bb0
zhusir <smallprogram@foxmail.com>
2024-07-17 14:11:18 +0800

    bash: update code

--- compile_script/copy_backgroundfiles.sh
+++ compile_script/copy_backgroundfiles.sh
@@ -3,7 +3,8 @@
 is_wsl2op=$1
 platform=$2
 
-copy_backgroundfiles_platforms=(X86 R5S R5C R4S R4SE R2S R2C H66K H68K H69K R66S R68S R_PI_3b R_PI_4b)
+source $GITHUB_WORKSPACE/compile_script/platforms.sh
+
 is_copy_backgroundfiles=false
 
 for p in "${copy_backgroundfiles_platforms[@]}"; do
--- compile_script/platforms.sh
+++ compile_script/platforms.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 
 platforms=(X86 R5S R5C R4S R4SE R2S R2C H66K H68K H69K R66S R68S R_PI_3b R_PI_4b Redmi_AX5)
+copy_backgroundfiles_platforms=(X86 R5S R5C R4S R4SE R2S R2C H66K H68K H69K R66S R68S R_PI_3b R_PI_4b)
