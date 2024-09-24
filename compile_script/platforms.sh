d64a577b5a445e5856180177c2d91e1e417b850e
zhusir <smallprogram@foxmail.com>
2024-09-24 22:42:48 +0800

    test

--- compile_script/platforms.sh
+++ compile_script/platforms.sh
@@ -1,5 +1,60 @@
 #!/bin/bash
 
-platforms=(X86 R5S R5C R4S R4SE R2S R2C H66K H68K H69K R66S R68S R_PI_3b R_PI_4b Asus_TUF_AX4200 JDCloud_AX6000 Phicomm_N1 Thunder_OneCloud RedMi_AX5 XiaoMi_AX6S XiaoMi_AX3000T XiaoMi_AX3600 XiaoMi_AX6000 XiaoMi_AX9000)
-# platforms=(X86 R5S R5C R4S R4SE R2S R2C H66K H68K H69K R66S R68S R_PI_3b R_PI_4b Asus_TUF_AX4200 JDCloud_AX6000 Phicomm_K3 Phicomm_N1 Thunder_OneCloud RedMi_AX5 XiaoMi_AX6S XiaoMi_AX3000T XiaoMi_AX3600 XiaoMi_AX6000 XiaoMi_AX9000)
-copy_backgroundfiles_platforms=(X86 R5S R5C R4S R4SE R2S R2C H66K H68K H69K R66S R68S R_PI_3b R_PI_4b)
+source_code_platforms=(immortalwrt openwrt lede)
+
+# 定义各平台的值
+immortalwrt_value='{
+    "REPO_URL": "https://github.com/immortalwrt/immortalwrt.git",
+    "REPO_BRANCH": "master",
+    "FEEDS_CONF": "feeds_config/immortalwrt.feeds.conf.default",
+    "CONFIGS": "config/immortalwrt_config",
+    "DIY_P1_SH": "diy_script/immortalwrt_diy/diy-part1.sh",
+    "DIY_P2_SH": "diy_script/immortalwrt_diy/diy-part2.sh"
+}'
+
+openwrt_value='{
+    "REPO_URL": "https://github.com/openwrt/openwrt.git",
+    "REPO_BRANCH": "master",
+    "FEEDS_CONF": "feeds_config/openwrt.feeds.conf.default",
+    "CONFIGS": "config/openwrt_config",
+    "DIY_P1_SH": "diy_script/openwrt_diy/diy-part1.sh",
+    "DIY_P2_SH": "diy_script/openwrt_diy/diy-part2.sh"
+}'
+
+lede_value='{
+    "REPO_URL": "https://github.com/coolsnowwolf/lede",
+    "REPO_BRANCH": "master",
+    "FEEDS_CONF": "feeds_config/lean.feeds.conf.default",
+    "CONFIGS": "config/leanlede_config",
+    "DIY_P1_SH": "diy_script/lean_diy/diy-part1.sh",
+    "DIY_P2_SH": "diy_script/lean_diy/diy-part2.sh"
+}'
+
+# 各个平台的设备列表
+immortalwrt_platforms=(X86 R2C R2CPLUS R2S R4S R4SE R4SENT R5C R5S R6C R6S R66S R68S)
+openwrt_platforms=(X86 R2C R2CPLUS R2S R4S R4SE R5C R5S R6C R6S)
+lede_platforms=(X86 R5S R5C R4S R4SE R2S R2C H66K H68K H69K R66S R68S R_PI_3b R_PI_4b Asus_TUF_AX4200 JDCloud_AX6000 Phicomm_N1 Thunder_OneCloud RedMi_AX5 XiaoMi_AX6S XiaoMi_AX3000T XiaoMi_AX3600 XiaoMi_AX6000 XiaoMi_AX9000)
+
+# 构建 JSON 矩阵
+matrix_json="["
+
+for source_platform in "${source_code_platforms[@]}"; do
+    # 根据 source_code_platforms 名称获取对应的变量名
+    platforms_var="${source_platform}_platforms[@]"
+    platforms=("${!platforms_var}")
+
+    # 获取对应的 value JSON 数据
+    value_var="${source_platform}_value"
+    value_json="${!value_var}"
+
+    for platform in "${platforms[@]}"; do
+        # 将每个 platform 添加到矩阵中，包含对应的 JSON 数据
+        matrix_json+="{\"source_code_platform\":\"${source_platform}\",\"platform\":\"${platform}\",\"value\":${value_json}},"
+    done
+done
+
+# 移除最后一个多余的逗号
+matrix_json="${matrix_json%,}]"
+
+# 输出到 GITHUB_OUTPUT
+echo "matrix=$matrix_json" >> "$GITHUB_OUTPUT"
