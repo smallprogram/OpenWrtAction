520a8089ca02a5bc5c541048235428e56e5e342b
zhusir <smallprogram@foxmail.com>
2024-09-25 16:52:25 +0800

    not complate

--- compile_script/step02_generate_release_tag.sh
+++ compile_script/step02_generate_release_tag.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-release_tag=$1
+source_code_platform=$1
+config=$2
 
 source $GITHUB_WORKSPACE/compile_script/platforms.sh
 
-openwrt_version=$(grep -o "DISTRIB_REVISION='[^']*'" $GITHUB_WORKSPACE/openwrt/package/lean/default-settings/files/zzz-default-settings | sed "s/DISTRIB_REVISION='\([^']*\)'/\1/")
+if [[ "$source_code_platform" == "immortalwrt" ]]; then
+  selected_platforms=("${immortalwrt_platforms[@]}")
+elif [[ "$source_code_platform" == "openwrt" ]]; then
+  selected_platforms=("${openwrt_platforms[@]}")
+elif [[ "$source_code_platform" == "lede" ]]; then
+  selected_platforms=("${lede_platforms[@]}")
+fi
 
-echo "[![](https://img.shields.io/github/downloads/smallprogram/OpenWrtAction/$release_tag/total?style=flat-square)](https://github.com/smallprogram/MyAction)" >>release.txt
-echo "### Firmware Information" >>release.txt
-echo "**:loudspeaker:Cancel the ext4 format and only keep the squashfs format.**" >>release.txt
-echo "**:computer:Including traditional IMG format firmware and UEFI boot firmware.**" >>release.txt
-echo "**:cd:Including qcow2 format firmware and UEFI boot firmware supporting pve virtual machine.**" >>release.txt
-echo "**:cd:Including vdi format firmware and UEFI boot firmware supporting Visual Box virtual machine.**" >>release.txt
-echo "**:cd:Including vhdx format firmware and UEFI boot firmware supporting Hyper-v virtual machines.**" >>release.txt
-echo "**:dvd:Including vmdk format firmware and UEFI boot firmware that support ESXi virtual machines (8.0 requires tool conversion).**" >>release.txt
-echo "### Openwrt Information" >>release.txt
-echo "**:minidisc: OpenWrt Version: $openwrt_version**" >>release.txt
-#echo "**:gear: Default-Setting Version: $PKG_VERSION.$PKG_RELEASE**" >> release.txt
-echo "### Compile Information" >>release.txt
-echo "platform|kernel|target|compile status" >>release.txt
-echo "-|-|-|-" >>release.txt
+echo "<table>">>${source_code_platform}_release.txt
+echo "  <tr>">>${source_code_platform}_release.txt
+echo "    <th>platform</th>">>${source_code_platform}_release.txt
+echo "    <th>source platform</th>">>${source_code_platform}_release.txt
+echo "    <th>kernel</th>">>${source_code_platform}_release.txt
+echo "    <th>target</th>">>${source_code_platform}_release.txt
+echo "    <th>compile status</th>">>${source_code_platform}_release.txt
+echo "  </tr>">>${source_code_platform}_release.txt
 
-for platform in "${platforms[@]}"; do
+for platform in "${selected_platforms[@]}"; do
     # Construct the path to the .config file
-    config_file="$GITHUB_WORKSPACE/config/leanlede_config/$platform.config"
+    config_file="$GITHUB_WORKSPACE/$config/$platform.config"
 
     # Extract the CONFIG_TARGET_BOARD value
     if [[ -f $config_file ]]; then
@@ -39,9 +40,17 @@ for platform in "${platforms[@]}"; do
     # Get the kernel version from the corresponding kernel file
     kernel_version=$(sed -n '2p' "$GITHUB_WORKSPACE/openwrt/include/kernel-$kernel" | awk -F '-' '{print $2}' | awk -F ' =' '{print $1}')
 
-    # Write the result to release.txt
-    echo "**:ice_cube: $platform**|**$kernel_version**|**$target_board**|![](https://img.shields.io/badge/build-in_progress_or_waiting.....-yellow?logo=githubactions&logoColor=yellow&style=flat-square)" >>release.txt
-done
 
-touch release.txt
+    echo "  <tr>">>${source_code_platform}_release.txt
+    echo "    <td>**:ice_cube: $platform**</td>">>${source_code_platform}_release.txt
+    echo "    <td>**$source_code_platform**</td>">>${source_code_platform}_release.txt
+    echo "    <td>**$kernel_version**</td>">>${source_code_platform}_release.txt
+    echo "    <td>**$target_board**</td>">>${source_code_platform}_release.txt
+    echo "    <td>![](https://img.shields.io/badge/build-in_progress_or_waiting.....-yellow?logo=githubactions&logoColor=yellow&style=flat-square)</td>">>${source_code_platform}_release.txt
+    echo "  </tr>">>${source_code_platform}_release.txt
+
+done
+echo "</table>">>${source_code_platform}_release.txt
+
+touch ${source_code_platform}_release.txt
 echo 'status=success' >>$GITHUB_OUTPUT
