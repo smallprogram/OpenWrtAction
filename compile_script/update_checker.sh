9bc9a59beb5592f8e04bf3d10990078dd7600bf6
zhusir <smallprogram@foxmail.com>
2024-09-29 02:03:30 +0800

    test

--- compile_script/update_checker.sh
+++ compile_script/update_checker.sh
@@ -6,11 +6,11 @@ num=$2
 resp="${url##*/}"
 
 cd $GITHUB_WORKSPACE
-rm -rf SHA_RESP
-mkdir -p SHA_RESP
+mkdir -p TMP_SHA_RESP
 cd $GITHUB_WORKSPACE/SHA_RESP
 git clone $url --filter=blob:none
 ls
 cd $resp
 echo "SHA_$num=$(git rev-parse --short HEAD)" >> $GITHUB_OUTPUT
 cd $GITHUB_WORKSPACE
+rm -rf TMP_SHA_RESP
