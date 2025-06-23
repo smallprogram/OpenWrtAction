06156cf860ea497f6b7a347e22a7c6550ea94632
zhusir <smallprogram@foxmail.com>
2025-06-23 11:38:37 +0800

    diyscript: optimize code

--- diy_script/immortalwrt_diy/diy-part2.sh
+++ diy_script/immortalwrt_diy/diy-part2.sh
@@ -17,6 +17,7 @@ sed -i 's/192.168.1.1/10.10.0.253/g' package/base-files/files/bin/config_generat
 
 
 # fixed rust host build download llvm in ci error
+sed -i 's/--set=llvm\.download-ci-llvm=false/--set=llvm.download-ci-llvm=true/' feeds/packages/lang/rust/Makefile
 grep -q -- '--ci false \\' feeds/packages/lang/rust/Makefile || sed -i '/x\.py \\/a \        --ci false \\' feeds/packages/lang/rust/Makefile
 
 
