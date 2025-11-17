0b33809e15ace2ef082c7d56e7929d9bfc008c91
zhusir <smallprogram@foxmail.com>
2025-11-17 11:01:40 +0800

    add custom feeds and packages

---
+++ diy_script/custom_feeds_and_packages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export repos=(
+  "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main"
+  "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main"
+  "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
+  "src-git helloworld https://github.com/fw876/helloworld;master"
+  "src-git OpenClash https://github.com/vernesong/OpenClash;master"
+  "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main"
+)
+
+clone_custom_packages () {
+    rm -rf ./package/custom_packages
+    mkdir -p ./package/custom_packages
+
+    git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/custom_packages/luci-theme-argon
+    git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/custom_packages/luci-app-argon-config
+    git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/custom_packages/luci-app-adguardhome
+    git clone https://github.com/sbwml/luci-app-mosdns -b v5 ./package/custom_packages/mosdns
+    git clone https://github.com/sirpdboy/luci-app-netspeedtest ./package/custom_packages/netspeedtest
+    git clone https://github.com/timsaya/openwrt-bandix.git ./package/custom_packages/openwrt-bandix
+    git clone https://github.com/timsaya/luci-app-bandix.git ./package/custom_packages/luci-app-bandix
+    git clone https://github.com/destan19/OpenAppFilter.git ./package/custom_packages/OpenAppFilter
+}
--- diy_script/openwrt_diy/diy-part1.sh
+++ diy_script/openwrt_diy/diy-part1.sh
@@ -10,6 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../custom_feeds_and_packages.sh"
+
 # ---------------------------------------------------------------feeds update---------------------------------------------------------------
 # 备份原始 feeds.conf.default
 cp feeds.conf.default feeds.conf.default.bak
@@ -18,17 +21,6 @@ cp feeds.conf.default feeds.conf.default.bak
 sed -i '/^#/d' feeds.conf.default
 sed -i -e 's|git.openwrt.org/feed|github.com/openwrt|g' -e 's|git.openwrt.org/project|github.com/openwrt|g' feeds.conf.default
 
-
-
-# 定义要添加的 feeds 数组
-repos=(
-  "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main"
-  "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main"
-  "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
-  "src-git helloworld https://github.com/fw876/helloworld;master"
-  "src-git OpenClash https://github.com/vernesong/OpenClash;master"
-  "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main"
-)
 
 # 创建临时文件
 temp_file=$(mktemp)
@@ -57,14 +49,4 @@ done
 echo "Updated feeds.conf.default"
 # ---------------------------------------------------------------end feeds update---------------------------------------------------------------
 
-rm -rf ./package/custom_packages
-mkdir -p ./package/custom_packages
-
-git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/custom_packages/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/custom_packages/luci-app-argon-config
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/custom_packages/luci-app-adguardhome
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 ./package/custom_packages/mosdns
-git clone https://github.com/sirpdboy/luci-app-netspeedtest ./package/custom_packages/netspeedtest
-git clone https://github.com/timsaya/openwrt-bandix.git ./package/custom_packages/openwrt-bandix
-git clone https://github.com/timsaya/luci-app-bandix.git ./package/custom_packages/luci-app-bandix
-git clone https://github.com/destan19/OpenAppFilter.git ./package/custom_packages/OpenAppFilter
+clone_custom_packages
