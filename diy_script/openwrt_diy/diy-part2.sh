7a94627561f7c92ce32e4a7a1abefd8c9a198223
smallprogram <smallprogram@foxmail.com>
2025-12-25 14:24:45 +0800

    diyscript: update code disable golang rust rust llvm patch-kernel patch

--- diy_script/openwrt_diy/diy-part2.sh
+++ diy_script/openwrt_diy/diy-part2.sh
@@ -18,15 +18,16 @@ sed -i 's/192.168.1.1/10.10.0.253/g' package/base-files/files/bin/config_generat
 
 
 rm -rf temp_resp
-git clone -b master --single-branch https://github.com/openwrt/packages.git temp_resp/openwrt_packages
-git clone -b main --single-branch https://github.com/openwrt/openwrt.git temp_resp/openwrt_source
+
+# git clone -b master --single-branch https://github.com/openwrt/packages.git temp_resp/openwrt_packages
+# git clone -b main --single-branch https://github.com/openwrt/openwrt.git temp_resp/openwrt_source
 
 # update golang version
-rm -rf feeds/packages/lang/golang
-cp -rf temp_resp/openwrt_packages/lang/golang feeds/packages/lang
-rm -rf feeds/packages/lang/rust
-cp -rf temp_resp/openwrt_packages/lang/rust feeds/packages/lang
-cp -rf temp_resp/openwrt_source/scripts/patch-kernel.sh scripts/
+# rm -rf feeds/packages/lang/golang
+# cp -rf temp_resp/openwrt_packages/lang/golang feeds/packages/lang
+# rm -rf feeds/packages/lang/rust
+# cp -rf temp_resp/openwrt_packages/lang/rust feeds/packages/lang
+# cp -rf temp_resp/openwrt_source/scripts/patch-kernel.sh scripts/
 
     
 git clone -b master --single-branch  https://github.com/immortalwrt/immortalwrt.git temp_resp/immortalwrt_source
@@ -34,8 +35,8 @@ git clone -b master --single-branch  https://github.com/immortalwrt/luci.git tem
 git clone -b master --single-branch  https://github.com/immortalwrt/packages.git temp_resp/immortalwrt_packages
 
 # add some package from immortalwrt
-cp -rf temp_resp/immortalwrt_packages/utils/coremark ./feeds/packages
-cp -rf temp_resp/immortalwrt_source/package/emortal/autocore ./package/emortal
+# cp -rf temp_resp/immortalwrt_packages/utils/coremark ./feeds/packages
+# cp -rf temp_resp/immortalwrt_source/package/emortal/autocore ./package/emortal
 cp -rf temp_resp/immortalwrt_luci/applications/luci-app-cifs-mount ./feeds/luci/applications
 cp -rf temp_resp/immortalwrt_luci/applications/luci-app-ddns-go ./feeds/luci/applications
 cp -rf temp_resp/immortalwrt_packages/net/ddns-go ./feeds/packages/net
@@ -54,8 +55,8 @@ rm -rf temp_resp
 ./scripts/feeds install -a
 
 # fixed rust host build download llvm in ci error
-sed -i 's/--set=llvm\.download-ci-llvm=false/--set=llvm.download-ci-llvm=true/' feeds/packages/lang/rust/Makefile
-grep -q -- '--ci false \\' feeds/packages/lang/rust/Makefile || sed -i '/x\.py \\/a \        --ci false \\' feeds/packages/lang/rust/Makefile
+# sed -i 's/--set=llvm\.download-ci-llvm=false/--set=llvm.download-ci-llvm=true/' feeds/packages/lang/rust/Makefile
+# grep -q -- '--ci false \\' feeds/packages/lang/rust/Makefile || sed -i '/x\.py \\/a \        --ci false \\' feeds/packages/lang/rust/Makefile
 
 
 # Add patches
