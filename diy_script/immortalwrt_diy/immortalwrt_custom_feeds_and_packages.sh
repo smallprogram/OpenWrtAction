694f3d30f7b544f8fb55bf743d9db7534f2d55f1
smallprogram <smallprogram@foxmail.com>
2025-12-27 13:22:39 +0800

    update X86.config from local bash

--- diy_script/immortalwrt_diy/immortalwrt_custom_feeds_and_packages.sh
+++ diy_script/immortalwrt_diy/immortalwrt_custom_feeds_and_packages.sh
@@ -18,7 +18,7 @@ clone_custom_packages () {
     git clone https://github.com/sirpdboy/luci-theme-kucat.git ./package/custom_packages/luci-theme-kucat
     git clone https://github.com/sirpdboy/luci-app-kucat-config.git ./package/custom_packages/luci-app-kucat-config
     git clone https://github.com/eamonxg/luci-theme-aurora.git ./package/custom_packages/luci-theme-aurora
-    git clone https://github.com/derisamedia/luci-theme-alpha.git ./package/custom_packages/luci-theme-alpha
+    git clone https://github.com/derisamedia/luci-theme-alpha-reborn.git ./package/custom_packages/luci-theme-alpha-reborn
     git clone https://github.com/animegasan/luci-app-alpha-config.git ./package/custom_packages/luci-app-alpha-config
     git clone https://github.com/AngelaCooljx/luci-theme-material3.git ./package/custom_packages/luci-theme-material3
     git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/custom_packages/luci-app-adguardhome
@@ -29,5 +29,5 @@ clone_custom_packages () {
     git clone https://github.com/destan19/OpenAppFilter.git ./package/custom_packages/OpenAppFilter
 
 
-    sed -i '/^[\t ]*PKG_VERSION:=/ s/\(PKG_VERSION:= *\)[^0-9.]*\([0-9.]*\)[^0-9.]*/\1\2/' "./package/custom_packages/luci-theme-alpha/Makefile"
+    sed -i '/^[\t ]*PKG_VERSION:=/ s/\(PKG_VERSION:= *\)[^0-9.]*\([0-9.]*\)[^0-9.]*/\1\2/' "./package/custom_packages/luci-theme-alpha-reborn/Makefile"
 }
