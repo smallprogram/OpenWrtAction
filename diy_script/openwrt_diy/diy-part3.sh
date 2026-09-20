c81c707dff912b3f253a29a630cddd75e4537387
David Mandy <smallprogramzhusir@gmail.com>
2026-09-20 16:50:14 +0800

    diyscript: optimize code

--- diy_script/openwrt_diy/diy-part3.sh
+++ diy_script/openwrt_diy/diy-part3.sh
@@ -42,6 +42,8 @@ sed -i "s/^SMARTDNS_WEBUI_SOURCE_VERSION:=.*/SMARTDNS_WEBUI_SOURCE_VERSION:=$LAT
 sed -i 's/^PKG_MIRROR_HASH:=.*/PKG_MIRROR_HASH:=/g' Makefile
 sed -i 's/^[[:space:]]*MIRROR_HASH:=.*/\tMIRROR_HASH:=/g' Makefile
 cd $current_path
+make package/custom_packages/openwrt-smartdns/clean
+rm -rf dl/smartdns*
 make defconfig > /dev/null
 make package/custom_packages/openwrt-smartdns/download -j8
 make package/custom_packages/openwrt-smartdns/check FIXUP=1 V=s
